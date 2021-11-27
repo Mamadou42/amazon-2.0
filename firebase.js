5762072815becf3701d523794e1d302dc756a9ee
mamadou42 <momocoly42@gmail.com>
2021-11-27 19:50:46 +0000

    checkout page

---
+++ firebase.js
@@ -0,0 +1,10 @@
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyDy9pldXDxsmJZqraBL5WBHDIFg1gQ6BuE",
+  authDomain: "fir-90eb9.firebaseapp.com",
+  projectId: "fir-90eb9",
+  storageBucket: "fir-90eb9.appspot.com",
+  messagingSenderId: "671404931810",
+  appId: "1:671404931810:web:80e5bca80434cfbac8fcbb",
+};
